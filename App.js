95a0fc17f45f1fa34346d0a455e794eb4b9edfca
Reshma888 <47694560+Reshma888@users.noreply.github.com>
2019-09-02 00:25:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './App.css';
+import Keypad from './components/Keypad';
+import Output from './components/Output';
+
+class App extends Component {
+  state={
+    result:''
+  }
+
+  buttonPress=(buttonName)=>{
+    if(buttonName === "=" ) {
+      this.calculate();
+    }else if(buttonName === "clear"){
+      this.reset()
+    }else{
+      this.setState({
+        result:this.state.result + buttonName
+      });
+    }
+  };
+  reset=() =>{
+    this.setState({
+      result: ''
+    });
+  };
+  calculate= () =>{
+    this.setState({
+      result: eval(this.state.result)
+    });
+  };
+  render(){
+    return(
+      <div className= "App">
+      <div className= "calculator">
+        <Output result= {this.state.result}/>
+        <Keypad buttonPress={this.buttonPress}/>
+        </div>
+        </div>
+    );
+  }
+}export default App;
+
+
+
